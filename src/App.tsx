b0326a8e64a86cc265dc88213044ea2764276d63
AJ Igherighe <the.coders.dao@gmail.com>
2022-10-17 09:03:35 -0400

    fresh app with working form

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,89 @@
-import React, { useState, useEffect } from 'react'
-import { useMultistepForm } from './useMultistepForm';
+import { FormEvent, useState } from "react"
+import { AccountForm } from './AccountForm';
+//import { AccountForm } from "./AccountForm"
+import { AddressForm } from "./AddressForm"
+import { useMultistepForm } from "./useMultistepForm"
+import { UserForm } from "./UserForm"
+import "./modern-normalize.css";
+import "./skeleton.css";
 
-const formData = [
-  {
-    id: 0,
-    labels: ["first", "last", "phone"],
-  },
-  {
-    id: 1,
-    labels: ["first", "last", "phone"],
-  },
-  {
-    id: 2,
-    labels: ["first", "last", "phone"],
-  },
-]
+type FormData = {
+  firstName: string
+  lastName: string
+  age: string
+  street: string
+  city: string
+  state: string
+  zip: string
+  email: string
+  password: string
+}
 
+const INITIAL_DATA: FormData = {
+  firstName: "",
+  lastName: "",
+  age: "",
+  street: "",
+  city: "",
+  state: "",
+  zip: "",
+  email: "",
+  password: "",
+}
 
-function App() {
-  const { steps } = useMultistepForm([]);
-  const [pageIndex, setPageIndex] = useState(0);
-  let currentPage = pageIndex + 1;
-  const formPages = formData.length;
+const [data, setData] = useState(INITIAL_DATA)
+  function updateFields(fields: Partial<FormData>) {
+    setData(prev => {
+      return { ...prev, ...fields }
+    })
+  }
+  const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } =
+    useMultistepForm([
+      <UserForm {...data} updateFields={updateFields} />,
+      <AddressForm {...data} updateFields={updateFields} />,
+      <AccountForm {...data} updateFields={updateFields} />,
+    ])
+
+  function onSubmit(e: FormEvent) {
+    e.preventDefault()
+    if (!isLastStep) return next()
+    alert("Successful Account Creation")
+  }
 
   return (
-    <div className="container" style={{ border: "1px border black" }}>
-      <div className="row">1/2</div>
-      <div className="row"></div>
-      <div className="row"></div>
-      <div className="row">
-        <button as="button">back</button>
-        <button as="button">next</button>
-      </div>
+    <div
+      style={{
+        position: "relative",
+        background: "white",
+        border: "1px solid black",
+        padding: "2rem",
+        margin: "1rem",
+        borderRadius: ".5rem",
+        fontFamily: "Arial",
+        maxWidth: "max-content",
+      }}
+    >
+      <form onSubmit={onSubmit}>
+        <div style={{ position: "absolute", top: ".5rem", right: ".5rem" }}>
+          {currentStepIndex + 1} / {steps.length}
+        </div>
+        {step}
+        <div
+          style={{
+            marginTop: "1rem",
+            display: "flex",
+            gap: ".5rem",
+            justifyContent: "flex-end",
+          }}
+        >
+          {!isFirstStep && (
+            <button type="button" onClick={back}>
+              Back
+            </button>
+          )}
+          <button type="submit">{isLastStep ? "Finish" : "Next"}</button>
+        </div>
+      </form>
     </div>
   )
 }
